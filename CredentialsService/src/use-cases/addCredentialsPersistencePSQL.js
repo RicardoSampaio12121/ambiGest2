1920222ff2115faaaafdc5e29259f6c629b22835
Ricardo José da Costa Sampaio <rsampaio@primeit.pt>
2022-11-25 19:25:07 +0000

    Add code when creating credentials

--- CredentialsService/src/use-cases/addCredentialsPersistencePSQL.js
+++ CredentialsService/src/use-cases/addCredentialsPersistencePSQL.js
@@ -1,28 +1,26 @@
 const { CredentialsEntity } = require("../entities/CredentialsEntity")
 const client = require("../framework/db/Postgresql/client")
-const {getClient, query, queryParams} = require("../framework/db/Postgresql/client")
+const { getClient, query, queryParams } = require("../framework/db/Postgresql/client")
 
-exports.addCredentialsPersistence = async(email, password) => {
+exports.addCredentialsPersistence = async (email, password) => {
 
     //Check if there are credentials already created with the same email
     getClient((errClient, client) => {
         queryParams("SELECT * FROM public.credentials WHERE email = $1;", [email], (err, res) => {
-            if(res.rows[0] != null){
-                return({status: '500', error: "This email is already in use."});
+            if (res.rows[0] != null) {
+                return ({ status: '500', error: "This email is already in use." });
             }
             client.end();
         }, client);
-    });
 
-     getClient((errClient, client) => {
-         queryParams('INSERT INTO public.credentials (email, password, verified) VALUES ($1, $2, $3);', [email, password, false], (err)=> {
-             if(err){
-                 return ({status: '500', error: err})
-             }
-             client.end();
-            
-             var a  = ({status: '200', data: email})
-             return a
-         }, client)
-     })
+        queryParams('INSERT INTO public.credentials (email, password, code, verified) VALUES ($1, $2, $3);', [email, password, (Math.random() + 1).toString(36).substring(7), false], (err) => {
+            if (err) {
+                return ({ status: '500', error: err })
+            }
+            client.end();
+
+            var a = ({ status: '200', data: email })
+            return a
+        }, client);
+    });
 }
