18a904cf7d7da84c52ae50d4a70ac5a0f8e3f149
Ricardo José da Costa Sampaio <rsampaio@primeit.pt>
2022-11-25 20:03:20 +0000

    dunno

---
+++ UsersService/api/controllers/routes/usersRoutes.js
@@ -0,0 +1,15 @@
+const router  = require('express').Router()
+
+const usersIterator = require("../../use-cases/usersIteratorMongoDB");
+
+router.route('/createUser')
+    .post(async (req, res) => {
+        const { name, surname, email, birthdate, role } = req.body;
+
+        try{
+            const output = usersIterator.createUserIterator(createUserPersistence, {name, surname, email, birthdate, role})
+            res.json(output);
+        }catch(error){
+            throw error;
+        }
+    })
---
+++ UsersService/api/use-cases/usersIteratorMongoDB.js
@@ -0,0 +1,9 @@
+"use strict"
+
+exports.createUserIterator = async({createUserPersistence}, {name, surname, email, birthdate, role}) => {
+    try{
+
+    }catch(error){
+        throw error;
+    }
+}
